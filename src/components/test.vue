c4a56a420dc8ff6d0195fb76783ff31897300a43
wenxiaoyan <1105047416@qq.com>
2019-08-13 16:58:53 +0800

    增加vue-router

---
+++ src/components/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{msg}}</div>
+</template>
+
+<script>
+export default {
+    name: "test",
+    data(){
+        return {
+            msg:'test'
+        }
+    }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuex from 'vuex'
+import VueRouter from 'vue-router'
+import test from './components/test.vue'
 
 Vue.config.productionTip = false
 Vue.use(Vuex)
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+    routes: [
+        { path: '/test', component: test }
+    ]
+})
 
 new Vue({
+    router,
     render: h => h(App),
 }).$mount('#app')
