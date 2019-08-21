b9d5deff9eb52b5d2865d17e3aa72f3b26da452c
wenxiaoyan <1105047416@qq.com>
2019-08-21 17:19:47 +0800

    增加echarts

---
+++ src/pages/echartsTest.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="index">
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "echartsTest",
+  data() {
+    return {
+    };
+  },
+  components: {
+
+  },
+  mounted() {
+  },
+  methods: {
+  }
+};
+</script>
+
+<style scoped lang="less">
+
+</style>
